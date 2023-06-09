1a2e14661fb01e95c24e33f2fd2bc1a04421127c
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-09 17:02:20 +0200

    push

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 console.log('JS ok')
 
+/******* | DOM'S ELEMENT | *******/
 
 const counter = document.getElementById('counter');
 const numbers = document.getElementById('numbers');
@@ -7,8 +8,10 @@ const result = document.getElementById('result');
 const firstInput = document.getElementById('first-input');
 const secondInput = document.getElementById('second-input');
 const thirdInput = document.getElementById('third-input');
-const fourdInput = document.getElementById('fourd-input');
+const fourthInput = document.getElementById('fourd-input');
 const buttonInput = document.getElementById('btn-send');
+
+/******* | FUNCTIONS | *******/
 
 function getRandomNumbers (maxNumber){
 
@@ -28,23 +31,26 @@ function getRandomNumbers (maxNumber){
     return randomNumbers;
 }
 
-function getInputUsernumbers (){
+function getInputUserNumbers (){
 
     const userNumbers = [];
 
-    firstInput.push(userNumbers);
-    secondInput.push(userNumbers);
-    thirdInput.push(userNumbers);
-    fourdInput.push(userNumbers);
+    userNumbers.push(parseInt(firstInput.value));
+    userNumbers.push(parseInt(secondInput.value));
+    userNumbers.push(parseInt(thirdInput.value));
+    userNumbers.push(parseInt(fourthInput.value));
 
     return userNumbers;
 }
 
 
-
+/******* | COUNTDOWN | *******/
 
 let timer = 5;
-numbers.innerHTML = getRandomNumbers(4).join(" ");
+randomNumbers = getRandomNumbers(4)
+numbers.innerHTML = randomNumbers.join(" ");
+
+console.log(randomNumbers)
 
 const reverse = setInterval(function(){
     const countDown = --timer
@@ -57,8 +63,22 @@ const reverse = setInterval(function(){
 
 }, 1000)
 
+/******* | BUTTON INPUT USER NUMBERS | *******/
+
 buttonInput.addEventListener('click', function (){
 
-   const userNumbers = getInputUsernumbers();
+    const userNumbers = getInputUserNumbers();
+    
+    for (let i = 0 ; i < randomNumbers.length; i++){
+        
+        foundNumbers = [];
+
+        if (userNumbers.includes(randomNumbers)){
+            foundNumbers.push(userNumbers);
+        }
+    
+        console.log(foundNumbers)
+    }
+
 
 });
